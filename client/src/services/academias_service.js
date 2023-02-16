45a5078b715aa0b4eaf7c96dab760c81fe119968
MarcelaPJ <marcela.p.j@gmail.com>
2023-02-15 22:07:36 -0300

    "Second commit"

--- client/src/services/academias_service.js
+++ client/src/services/academias_service.js
@@ -10,7 +10,7 @@ export const updateAcademy = (id, academia) => axios.put('http://localhost:8000/
 
 export const deleteAcademy = (id) => axios.delete('http://localhost:8000/api/academias/' + id);
 
-export const createUser = (name, email, password, confirmPassword) => axios.post('http://localhost:8000/api/academias/register', { name, email, password, confirmPassword });
+export const createUser = (name, email, password, confirmPassword) => axios.post('http://localhost:8000/api/academias/register', { name, lastName:name, email, password, confirmPassword });
 
 export const loginUser = (email, password) => axios.post('http://localhost:8000/api/academias/login', { email, password }, { withCredentials: true });
 
